697235c943a3ce39d5547b04d273f7af6d134a73
Manojemjay <manojjmanu7@gmail.com>
2022-06-11 23:57:31 +0530

    added Q4) Count distinct pairs with difference k

--- Hashing3.java
+++ Hashing3.java
@@ -37,3 +37,69 @@ public class Hashing3 {
 }
     
 }
+
+
+/*import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Main
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+      Scanner sc = new Scanner(System.in);
+      int n=sc.nextInt(), k = sc.nextInt();
+      int arr[] = new int[n];
+      for(int i=0;i<n;i++){
+        arr[i] = sc.nextInt();
+      }
+      int count=0;
+      for(int i=0;i<n-1;i++){
+        for(int j=i+1;j<n;j++){
+          int diff=0;
+          diff = Math.abs(arr[i]-arr[j]);
+        //  System.out.println(diff);
+          if(diff==k){
+            count++;
+         //   System.out.println(count);
+            }
+        }
+      }
+      System.out.println(count);
+      
+	}
+}*/
+
+
+ class hashing4 {
+    public static void main(String args[]) {
+      Scanner input = new Scanner(System.in);
+      int n =input.nextInt(), k = input.nextInt();
+      int a[] = new int[n];
+      for(int i = 0; i < n; i++){
+        a[i] = input.nextInt();
+      }
+      HashMap<Integer, Integer> map = new HashMap();
+      int ans = 0;
+      for(int i = 0; i < n; i++){
+        int cnt = 0;
+        if(map.containsKey(a[i])){
+          cnt = map.get(a[i]);
+        }
+        cnt++;
+        map.put(a[i], cnt);
+      }
+      for(int i = 0; i < n; i++){
+        if(map.containsKey(a[i] - k)){
+
+          if(k != 0 || map.get(a[i] - k) > 1){
+          
+            ans++;
+           // System.out.println(ans);
+            map.remove(a[i] - k);
+          }
+        }
+      }
+      System.out.println(ans);
+    }
+}
